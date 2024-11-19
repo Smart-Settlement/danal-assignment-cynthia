d53564a1bd21079fba97d723845f9d43f28a7dbc
cynthia <feiver@naver.com>
2024-11-19 21:45:39 +0900

    init : Spring Boot 애플리케이션, MySQL 데이터베이스 연결 및 Spring Batch 기본 설정

---
+++ src/main/java/com/publicdatabatchimporter/publicdatabatchimporter/PublicDataBatchImporterApplication.java
@@ -0,0 +1,13 @@
+package com.publicdatabatchimporter.publicdatabatchimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicDataBatchImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicDataBatchImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicdatabatchimporter/publicdatabatchimporter/PublicDataBatchImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicdatabatchimporter.publicdatabatchimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicDataBatchImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
